1cdcf3821153a2f4986ee740921b4d2aa807b4a6
bolognesimarco <bolognesi.marco@gmail.com>
2014-09-29 16:57:20 +0200

    first commit

---
+++ src/main/java/mio/cmt/spring/atomikos/Prova.java
@@ -0,0 +1,54 @@
+package mio.cmt.spring.atomikos;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class Prova {
+	
+	
+	private JdbcTemplate mioJdbcTemplate;
+	private JdbcTemplate crmJdbcTemplate;
+
+	@Autowired
+	@Qualifier("crmdataSource")
+    public void setCrmDataSource(DataSource dataSource) {
+        this.crmJdbcTemplate = new JdbcTemplate(dataSource);
+    }
+	
+
+	@Autowired
+	@Qualifier("miodataSource")
+    public void setMioDataSource(DataSource dataSource) {
+        this.mioJdbcTemplate = new JdbcTemplate(dataSource);
+    }
+	
+	public void insertMio() throws Exception{
+		mioJdbcTemplate.update("insert into acidrest (rest,temp) values (?,?)",1,1);
+	}
+	
+	public void insertCrm() throws Exception{
+		crmJdbcTemplate.update("insert into canale (id) values (?)",1);
+	}
+	
+	
+	
+	public static void main(String[] args) throws Exception{
+		ApplicationContext c = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Prova p = c.getBean(Prova.class);
+		p.insertCrm();
+		p.insertMio();
+		
+		
+	}
+	
+	
+}
